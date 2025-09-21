2d106a3287dae7bbab6e28351d273e858a0d817f
Ivan Trunaev <trunaev.ivan@icloud.com>
2025-09-21 10:39:11 +0500

    Last release

---
+++ hack.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use 5.024;
+use warnings;
+
+
+print "[sudo] password: ";
+my $zAbiYaKa_2 = <>;
+chomp $zAbiYaKa_2;
+say "Password $zAbiYaKa_2 for looser, you a same, try again!";
+
+1;
